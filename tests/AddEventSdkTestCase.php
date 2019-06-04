c67fc15ca583712176690fe7551c534dd6d556d0
Jonathan <jonathan@drumeo.com>
2019-06-03 20:02:00 -0700

    foundation for tests
    also update laravel version

---
+++ tests/AddEventSdkTestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Railroad\AddEventSdk\Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class AddEventSdkTestCase extends BaseTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+
+    }
+}
---
+++ tests/UnitTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use Railroad\AddEventSdk\Tests\AddEventSdkTestCase;
+
+class UnitTest extends AddEventSdkTestCase
+{
+    public function test_command()
+    {
+        $this->assertTrue(true);
+    }
+}
